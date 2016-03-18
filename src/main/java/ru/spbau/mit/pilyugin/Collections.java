efe392b628362697138c95041b984dd1857ba47d
Kirill Pilyugin <kirillpilyugin@gmail.com>
2016-03-18 23:14:40 +0300

    implement functional java task

---
+++ src/main/java/ru/spbau/mit/pilyugin/Collections.java
@@ -0,0 +1,67 @@
+package ru.spbau.mit.pilyugin;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Collections {
+
+    public static <A, R> List<R> map(Function1<? super A, ? extends R> function, Iterable<? extends A> iterable) {
+        List<R> result = new ArrayList<>();
+        for (A element : iterable) {
+            result.add(function.apply(element));
+        }
+        return result;
+    }
+
+    public static <A> List<A> filter(Predicate<? super A> predicate, Iterable<? extends A> iterable) {
+        List<A> result = new ArrayList<>();
+        for (A element : iterable) {
+            if (predicate.apply(element)) {
+                result.add(element);
+            }
+        }
+        return result;
+    }
+
+    public static <A> List<A> takeWhile(Predicate<? super A> predicate, Iterable<? extends A> iterable) {
+        List<A> result = new ArrayList<>();
+        for (A element : iterable) {
+            if (!predicate.apply(element)) {
+                break;
+            }
+            result.add(element);
+        }
+        return result;
+    }
+
+    public static <A> List<A> takeUnless(Predicate<? super A> predicate, Iterable<? extends A> elements) {
+        return takeWhile(Predicate.not(predicate), elements);
+    }
+
+    public static <A, R> R foldr(Function2<? super A, ? super R, ? extends R> function,
+                                 R initialValue, Iterable<? extends A> iterable) {
+        return processFoldr(function, initialValue, iterable.iterator());
+    }
+
+    public static <A, R> R foldl(Function2<? super R, ? super A, ? extends R> function,
+                                 R initialValue, Iterable<? extends A> iterable) {
+        return processFoldl(function, initialValue, iterable.iterator());
+    }
+
+    public static <A, R> R processFoldr(Function2<? super A, ? super R, ? extends R> function,
+                                        R initialValue, Iterator<? extends A> iterator) {
+        if (!iterator.hasNext()) {
+            return initialValue;
+        }
+        return function.apply(iterator.next(), processFoldr(function, initialValue, iterator));
+    }
+
+    public static <A, R> R processFoldl(Function2<? super R, ? super A, ? extends R> function,
+                                        R initialValue, Iterator<? extends A> iterator) {
+        if (!iterator.hasNext()) {
+            return initialValue;
+        }
+        return processFoldl(function, function.apply(initialValue, iterator.next()), iterator);
+    }
+}
---
+++ src/main/java/ru/spbau/mit/pilyugin/Function2.java
@@ -0,0 +1,25 @@
+package ru.spbau.mit.pilyugin;
+
+public interface Function2<A1, A2, R> {
+
+    R apply(A1 arg1, A2 arg2);
+
+    // g(f(x, y))
+    default <R1> Function2<A1, A2, R1> compose(Function1<? super R, ? extends R1> g) {
+        return (arg1, arg2) -> g.apply(apply(arg1, arg2));
+    }
+
+    // f(_, y)
+    default Function1<A2, R> bind1(A1 arg1) {
+        return arg2 -> apply(arg1, arg2);
+    }
+
+    // f(x, _)
+    default Function1<A1, R> bind2(A2 arg2) {
+        return arg1 -> apply(arg1, arg2);
+    }
+
+    default Function1<A1, Function1<A2, R>> curry() {
+        return arg1 -> arg2 -> apply(arg1, arg2);
+    }
+}
