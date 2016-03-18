d7f66f380fc7ddfe8b9a53e76b30d00f76667c2f
Kirill Pilyugin <kirillpilyugin@gmail.com>
2016-03-18 23:59:32 +0300

    predicate constants are static methods instead of fields

--- src/main/java/ru/spbau/mit/pilyugin/Predicate.java
+++ src/main/java/ru/spbau/mit/pilyugin/Predicate.java
@@ -2,18 +2,23 @@ package ru.spbau.mit.pilyugin;
 
 public interface Predicate<A> extends Function1<A, Boolean> {
 
-    Predicate<?> ALWAYS_TRUE = (value) -> true;
-    Predicate<?> ALWAYS_FALSE = (value) -> false;
-
     default Predicate<A> and(Predicate<? super A> other) {
-        return (value) -> apply(value) && other.apply(value);
+        return value -> apply(value) && other.apply(value);
     }
 
     default Predicate<A> or(Predicate<? super A> other) {
-        return (value) -> apply(value) || other.apply(value);
+        return value -> apply(value) || other.apply(value);
     }
 
     static <A> Predicate<A> not(Predicate<? super A> other) {
-        return (value) -> !other.apply(value);
+        return value -> !other.apply(value);
+    }
+
+    static <A> Predicate<A> alwaysTrue() {
+        return value -> true;
+    }
+
+    static <A> Predicate<A> alwaysFalse() {
+        return value -> false;
     }
 }
