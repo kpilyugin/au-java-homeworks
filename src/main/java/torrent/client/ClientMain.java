3b8de6eeb463b3c3576322a984c22f647e04e137
Kirill Pilyugin <kirillpilyugin@gmail.com>
2016-11-05 20:11:28 +0300

    minor formatting

--- src/main/java/torrent/client/ClientMain.java
+++ src/main/java/torrent/client/ClientMain.java
@@ -12,52 +12,47 @@ import java.util.concurrent.ExecutionException;
 
 public class ClientMain {
     public static void main(String[] args) throws InterruptedException, IOException, ExecutionException {
-        TorrentClient client = null;
         if (args.length < 2) {
             System.out.println("Usage: <tracker_ip> <port>");
             return;
         }
         InetSocketAddress trackerAddress = new InetSocketAddress(InetAddress.getByName(args[0]), TorrentTracker.PORT);
         int port = Integer.parseInt(args[1]);
-        try {
-            client = new TorrentClient(trackerAddress, port);
-            try (Scanner scanner = new Scanner(System.in)) {
-                while (scanner.hasNextLine()) {
-                    String line = scanner.nextLine();
-                    String[] cmd = line.split(" ");
-                    try {
-                        switch (cmd[0]) {
-                            case "list":
-                                List<FileInfo> files = client.listFiles();
-                                System.out.println("Number of files = " + files.size());
-                                for (int i = 0; i < files.size(); i++) {
-                                    FileInfo file = files.get(i);
-                                    System.out.println((i + 1) + ": " + "id = " + file.getId() +
-                                            ", name = " + file.getName() + ", size = " + file.getSize());
-                                }
-                                break;
-                            case "add":
-                                String fileName = cmd[1];
-                                client.addFile(fileName);
-                                break;
-                            case "get":
-                                int id = Integer.parseInt(cmd[1]);
-                                client.getFile(id);
-                                break;
-                            case "exit":
-                                return;
-                            default:
-                                printUsage();
-                        }
-                    } catch (Exception e) {
-                        System.err.println("Failed to execute command");
-                        e.printStackTrace();
+        TorrentClient client = new TorrentClient(trackerAddress, port);
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] cmd = line.split(" ");
+                try {
+                    switch (cmd[0]) {
+                        case "list":
+                            List<FileInfo> files = client.listFiles();
+                            System.out.println("Number of files = " + files.size());
+                            for (int i = 0; i < files.size(); i++) {
+                                FileInfo file = files.get(i);
+                                System.out.println((i + 1) + ": " + "id = " + file.getId() +
+                                        ", name = " + file.getName() + ", size = " + file.getSize());
+                            }
+                            break;
+                        case "add":
+                            String fileName = cmd[1];
+                            client.addFile(fileName);
+                            break;
+                        case "get":
+                            int id = Integer.parseInt(cmd[1]);
+                            client.getFile(id);
+                            break;
+                        case "exit":
+                            client.end();
+                            return;
+                        default:
+                            printUsage();
                     }
+                } catch (Exception e) {
+                    System.err.println("Failed to execute command");
+                    e.printStackTrace();
                 }
-            }
-        } finally {
-            if (client != null) {
-                client.end();
             }
         }
     }
